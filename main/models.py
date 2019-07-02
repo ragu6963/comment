d40b176ffbde1f86e47ff6aecda5f7c6b57f90cb
ragu6963 <bhj1684@naver.com>
2019-07-02 20:06:06 +0900

    first

---
+++ main/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User  
+
+class Post(models.Model): 
+    user = models.ForeignKey(User,on_delete=models.CASCADE) 
+    title = models.CharField(max_length = 200)
+    content = models.TextField(blank = True)
+    created_at = models.DateTimeField(auto_now_add= True)
+    updated_at = models.DateTimeField(auto_now = True)  
---
+++ main/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, redirect 
+from .models import Post
+
+def index(request):
+    posts = Post.objects.all() 
+    context= {
+        "posts":posts
+    } 
+    return render(request,'index.html',context)
+    
+def read(request,post_id):
+    post = Post.objects.get(id=post_id)
+    context = {
+        "post":post
+    }
+    return render(request,'read.html',context)
+
+def create(request): 
+    if request.method == "GET":
+        return render(request,'create.html') 
+
+    elif request.method == "POST":
+        post = Post() 
+        post.user = request.user 
+        post.title = request.POST['title']
+        post.content = request.POST['content']  
+        post.save() 
+        return redirect(index) 
+
+def update(request,post_id):
+    if request.method == "GET":
+        post = Post.objects.get(id=post_id)
+        context={
+            "post":post
+        }
+        return render(request,'update.html',context)
+    
+    elif request.method == "POST":  
+        post = Post.objects.get(id=post_id)
+        post.title = request.POST['title']
+        post.content = request.POST['content'] 
+        post.save() 
+        return redirect(index) 
+
+def delete(request,post_id):
+    post = Post.objects.get(id = post_id)
+    post.delete()
+    return redirect(index)
